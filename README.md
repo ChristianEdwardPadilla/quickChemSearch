be7267702558e41c18ded8e911e2e3e1d75b0266
ChristianEdwardPadilla <christianepadilla@gmail.com>
2019-06-13 16:03:35 -0500

    crawler removed

--- README.md
+++ README.md
@@ -1,7 +1,2 @@
-need to have redis-server installed alongside node etc.
-sudo apt-get redis-server
-
-useful commands to run on command line with redis:
-redis-cli //access redis command line interface
-KEYS \* //show all keys in this db
-HGETALL example //get all key/value pairs in example hashmap
+molecular search tool front-end
+DB and crawler backend not available
--- server.js
+++ server.js
@@ -1,45 +1,43 @@
-const express = require("express");
-const path = require("path");
-const fs = require("fs");
-const redis = require("redis");
-const fetch = require("node-fetch");
-const bodyParser = require("body-parser");
-const cookieParser = require("cookie-parser");
-
-const mockdb = require("./mockdb.js");
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const redis = require('redis');
+const fetch = require('node-fetch');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 const client = redis.createClient();
 
-client.on("connect", () => {
-  console.log("connected to Redis...");
+client.on('connect', () => {
+  console.log('connected to Redis...');
 });
 
-app.use("/", express.static("client"));
+app.use('/', express.static('client'));
 app.use(bodyParser.json());
 
-app.post("/submitChem", (req, res) => {
-  console.log("post request recieved");
+app.post('/submitChem', (req, res) => {
+  console.log('post request recieved');
 
   //catch eroneous or potentially malicious post requests
-  if (!req.body.smiles || typeof req.body.smiles !== "string") {
-    console.log("bad post request caught");
+  if (!req.body.smiles || typeof req.body.smiles !== 'string') {
+    console.log('bad post request caught');
     res.send();
     return;
   }
 
-  // now check if the incoming request is already in my redis database
-  client.hget("smilesHash", req.body.smiles, (err, result) => {
+  // now check if the incoming request is already in redis database
+  client.hget('smilesHash', req.body.smiles, (err, result) => {
     if (err) {
-      console.log("Redis hget error for req.body.smiles: ", req.body.smiles);
+      console.log('Redis hget error for req.body.smiles: ', req.body.smiles);
       queryAPI();
       return;
     } else if (!result) {
-      console.log("No such redis entry: ", req.body.smiles);
+      console.log('No such redis entry: ', req.body.smiles);
       queryAPI();
       return;
     } else {
-      console.log("Got redis entry for: ", req.body.smiles);
+      console.log('Got redis entry for: ', req.body.smiles);
       res.send(result);
       return;
     }
@@ -48,25 +46,17 @@ app.post("/submitChem", (req, res) => {
   function queryAPI() {
     //if the request has made it this far it should be a new smiles entry,
     //therefore we need to query the api and store the result in the redis client
-    fetch(
-      `https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/smiles/${
-        req.body.smiles
-      }/property/MolecularFormula,MolecularWeight,InChIKey/JSON`
-    )
+    fetch(`https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/smiles/${req.body.smiles}/property/MolecularFormula,MolecularWeight,InChIKey/JSON`)
       .then(res => res.json())
       .then(data => {
         const pubchemResponse = data.PropertyTable.Properties[0];
-        console.log("output from pubchem api fetch: ", pubchemResponse);
+        console.log('output from pubchem api fetch: ', pubchemResponse);
         res.send(pubchemResponse);
-        client.hset(
-          "smilesHash",
-          req.body.smiles,
-          JSON.stringify(pubchemResponse)
-        );
+        client.hset('smilesHash', req.body.smiles, JSON.stringify(pubchemResponse));
       });
   }
 });
 
 app.listen(3000, () => {
-  console.log("listening on port 3000");
+  console.log('listening on port 3000');
 });
